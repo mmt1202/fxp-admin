040e87308e2e6a7dafb0361c93f84b87a0e61d2b
木木糖 <106941579+mmt1202@users.noreply.github.com>
2026-06-24 17:31:58 +0800

    feat: export migrated api helpers from index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,6 @@
 export * from './client';
 export * from './geo';
+export * from './prompts';
+export * from './security';
+export * from './migratedFeatures';
 export * as adminApiModules from './modules';
